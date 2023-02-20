6f3f694e0d1f71326571b1ead2884ca9a321476c
Enes Keskin <eneoxkeskin@gmail.com>
2023-02-20 14:03:51 +0300

    Advanced_Musteri_Segmentasyon
    Advanced_Musteri_Segmentasyon

---
+++ cusmtomer_segmentation.py
@@ -0,0 +1,92 @@
+import numpy as np
+import pandas as pd
+
+from sklearn.preprocessing import MinMaxScaler
+from matplotlib import pyplot as plt
+
+from kmodes.kprototypes import KPrototypes
+from kmodes.kmodes import KModes
+
+
+df = pd.read_csv("segmentation_data.csv")
+df.head()
+
+
+df.tail()
+
+df.isnull().sum()
+
+
+df_temp = df[['ID', 'Age', 'Income']]
+df_temp
+
+scaler = MinMaxScaler()
+
+scaler.fit(df[['Age']])
+df['Age'] = scaler.transform(df[['Age']])
+
+scaler.fit(df[['Income']])
+df['Income'] = scaler.transform(df[['Income']])
+
+df = df.drop(['ID'], axis=1)
+
+
+mark_array = df.values
+
+mark_array[:, 2] = mark_array[:, 2].astype(float)
+mark_array[:, 4] = mark_array[:, 4].astype(float)
+
+df.head()
+
+kproto = KPrototypes(n_clusters=10, verbose=2, max_iter=20)
+clusters = kproto.fit_predict(mark_array, categorical=[0, 1, 3, 5, 6])
+
+
+print(kproto.cluster_centroids_)
+
+len(kproto.cluster_centroids_)
+
+
+cluster_dict = []
+for c in clusters:
+    cluster_dict.append(c)
+
+
+df['cluster'] = cluster_dict
+
+
+df[['ID', 'Age', 'Income']] = df_temp
+
+
+df[df['cluster'] == 0].head(10)
+
+
+df1 = df[df.cluster == 0]
+df2 = df[df.cluster == 1]
+df3 = df[df.cluster == 2]
+df4 = df[df.cluster == 3]
+df5 = df[df.cluster == 4]
+df6 = df[df.cluster == 6]
+df7 = df[df.cluster == 7]
+df8 = df[df.cluster == 8]
+df9 = df[df.cluster == 9]
+df10 = df[df.cluster == 10]
+
+
+plt.figure(figsize=(15, 15))
+plt.xlabel('Age')
+plt.ylabel('Income')
+
+plt.scatter(df1.Age, df1['Income'], color='green', alpha=0.4)
+plt.scatter(df2.Age, df2['Income'], color='red', alpha=0.4)
+plt.scatter(df3.Age, df3['Income'], color='gray', alpha=0.4)
+plt.scatter(df4.Age, df4['Income'], color='orange', alpha=0.4)
+plt.scatter(df5.Age, df5['Income'], color='yellow', alpha=0.4)
+plt.scatter(df6.Age, df6['Income'], color='cyan', alpha=0.4)
+plt.scatter(df7.Age, df7['Income'], color='magenta', alpha=0.4)
+plt.scatter(df8.Age, df8['Income'], color='gray', alpha=0.4)
+plt.scatter(df9.Age, df9['Income'], color='purple', alpha=0.4)
+plt.scatter(df10.Age, df10['Income'], color='blue', alpha=0.4)
+
+plt.legend()
+plt.show()
